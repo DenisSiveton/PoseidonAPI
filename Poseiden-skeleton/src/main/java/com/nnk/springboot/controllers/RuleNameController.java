66a1bc0cd1108181fcf42d481e87c25a7d67466b
Denis Siveton <denis.siveton@hotmail.com>
2023-06-15 17:47:24 +0200

    JIRA Story 4 - Setup RuleName Controller in controllers package

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,42 +16,60 @@ import javax.validation.Valid;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+    // TODO: Inject RuleName service --> DONE
+    @Autowired
+    private RuleNameRepository ruleNameRepository;
 
     @RequestMapping("/ruleName/list")
     public String home(Model model)
     {
-        // TODO: find all RuleName, add to model
+        // TODO: find all RuleName, add to model --> DONE
+        model.addAttribute("ruleNames", ruleNameRepository.findAll());
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(RuleName ruleName) {
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
     public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
+        // TODO: check data valid and save to db, after saving return RuleName list --> DONE
+        if (!result.hasErrors()) {
+            ruleNameRepository.save(ruleName);
+            model.addAttribute("ruleNames", ruleNameRepository.findAll());
+            return "redirect:/ruleName/list";
+        }
         return "ruleName/add";
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+        // TODO: get RuleName by Id and to model then show to the form --> DONE
+        RuleName ruleName = ruleNameRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid ruleName Id:" + id));
+        model.addAttribute("ruleName", ruleName);
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
     public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+        // TODO: check required fields, if valid call service to update RuleName and return RuleName list --> DONE
+        if (result.hasErrors()) {
+            return "ruleName/update";
+        }
+        ruleNameRepository.save(ruleName);
+        model.addAttribute("ruleNames", ruleNameRepository.findAll());
         return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list --> DONE
+        RuleName ruleName = ruleNameRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid ruleName Id:" + id));
+        ruleNameRepository.delete(ruleName);
+        model.addAttribute("ruleNames", ruleNameRepository.findAll());
         return "redirect:/ruleName/list";
     }
 }
