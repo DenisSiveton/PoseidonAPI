fd61adf4d3b8f1f99b715d4529a00c53082dcde3
Denis Siveton <denis.siveton@hotmail.com>
2023-09-11 10:37:04 +0200

    Implement unit test for Trade repository layer

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/UnitTest/TradeTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/UnitTest/TradeTests.java
@@ -1,46 +1,110 @@
-package com.nnk.springboot;
+package com.nnk.springboot.UnitTest;
 
+import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.repositories.TradeRepository;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class TradeTests {
 
 	@Autowired
 	private TradeRepository tradeRepository;
 
+	private Trade tradeTest;
+
+	@BeforeEach
+	public void setupTestData(){
+		// ARRANGE
+		tradeTest = new Trade("account Test", "type Test", 32.0);
+	}
+
 	@Test
-	public void tradeTest() {
-		Trade trade = new Trade("Trade Account", "Type");
+	@DisplayName("Test that saves a Trade")
+	public void givenBidListObject_whenSave_thenReturnSavedBidList() {
+		// ACT
+		Trade savedTrade = tradeRepository.save(tradeTest);
 
-		// Save
-		trade = tradeRepository.save(trade);
-		Assert.assertNotNull(trade.getId());
-		Assert.assertTrue(trade.getAccount().equals("Trade Account"));
+		//ASSERT
+		assertThat(savedTrade).isNotNull();
+		assertThat(savedTrade.getId()).isGreaterThan(0);
+		assertThat(savedTrade.getAccount()).isEqualTo("account Test");
 
-		// Update
-		trade.setAccount("Trade Account Update");
-		trade = tradeRepository.save(trade);
-		Assert.assertTrue(trade.getAccount().equals("Trade Account Update"));
+	}
 
-		// Find
-		List<Trade> listResult = tradeRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+	@Test
+	@DisplayName("Test that gets a list of Trade")
+	public void givenBidListList_whenFindAll_thenReturnBidList(){
+		// ARRANGE
+		Trade tradeOne = new Trade("account 1 Test", "type 1 Test", 34.0);
+		Trade tradeTwo = new Trade("account 2 Test", "type 2 Test", 35.0);
 
-		// Delete
-		Integer id = trade.getId();
-		tradeRepository.delete(trade);
-		Optional<Trade> tradeList = tradeRepository.findById(id);
-		Assert.assertFalse(tradeList.isPresent());
+		tradeRepository.save(tradeOne);
+		tradeRepository.save(tradeTwo);
+
+		// ACT
+		List<Trade> tradeList = tradeRepository.findAll();
+
+		// ASSERT
+		assertThat(tradeList).isNotEmpty();
+		assertThat(tradeList.size()).isEqualTo(2);
+		assertThat(tradeList.get(1).getBuyQuantity()).isEqualTo(35.0);
+	}
+
+	@Test
+	@DisplayName("Test to get Trade by Id")
+	public void givenBidListObject_whenFindById_thenReturnBidListObject() {
+		// ARRANGE
+		tradeRepository.save(tradeTest);
+
+		// ACT
+		Trade getTrade = tradeRepository.findById(tradeTest.getId()).get();
+
+		// ASSERT
+		assertThat(getTrade).isNotNull();
+		assertThat(getTrade.getType()).isEqualTo("type Test");
+	}
+
+	@Test
+	@DisplayName("Test : get Trade update operation")
+	public void givenEmployeeObject_whenUpdate_thenEmployeeObject() {
+		// ARRANGE
+		tradeRepository.save(tradeTest);
+
+		// ACT
+		Trade getTrade = tradeRepository.findById(tradeTest.getId()).get();
+
+		getTrade.setAccount("account updated Test");
+		getTrade.setType("type updated Test");
+
+		Trade updatedTrade = tradeRepository.save(getTrade);
+
+		// ASSERT
+		assertThat(updatedTrade).isNotNull();
+		assertThat(updatedTrade.getAccount()).isEqualTo("account updated Test");
+		assertThat(updatedTrade.getType()).isEqualTo("type updated Test");
+	}
+
+	@Test
+	@DisplayName("Test : delete Trade operation")
+	public void givenEmployeeObject_whenDelete_thenRemoveEmployee() {
+		// ARRANGE
+		Integer id = tradeRepository.save(tradeTest).getId();
+
+		// ACT
+		tradeRepository.deleteById(id);
+		Optional<Trade> deleteTrade = tradeRepository.findById(id);
+
+		// ASSERT
+		assertThat(deleteTrade).isEmpty();
 	}
 }
