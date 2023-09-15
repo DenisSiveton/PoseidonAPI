2419a7c08352ecc93bf8503bb94014148bb31edc
Denis Siveton <denis.siveton@hotmail.com>
2023-09-15 18:03:35 +0200

    Implement integration test for CurvePoint controller layer

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -15,28 +15,28 @@ public class CurvePoint {
     // TODO: Map columns in data table CURVEPOINT with corresponding java fields --> DONE
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "Id")
+    @Column (name = "id")
     private int id;
 
-    @Column (name = "CurveId")
+    @Column (name = "curve_id")
     @NotNull(message = "Must not be null")
     private Integer curveId;
 
-    @Column (name = "asOfDate")
+    @Column (name = "as_Of_date")
     private String asOfDate;
 
     @Column (name = "term")
-    private double term;
+    private Double term;
 
     @Column (name = "value")
-    private double value;
+    private Double value;
 
-    @Column (name = "creationDate")
+    @Column (name = "creation_date")
     private String creationDate;
 
     public CurvePoint() {}
 
-    public CurvePoint(int curveId, double term, double value) {
+    public CurvePoint(Integer curveId, Double term, Double value) {
         this.curveId = curveId;
         this.term = term;
         this.value = value;
@@ -50,11 +50,11 @@ public class CurvePoint {
         this.id = id;
     }
 
-    public int getCurveId() {
+    public Integer getCurveId() {
         return curveId;
     }
 
-    public void setCurveId(int curveId) {
+    public void setCurveId(Integer curveId) {
         this.curveId = curveId;
     }
 
@@ -66,19 +66,19 @@ public class CurvePoint {
         this.asOfDate = asOfDate;
     }
 
-    public double getTerm() {
+    public Double getTerm() {
         return term;
     }
 
-    public void setTerm(double term) {
+    public void setTerm(Double term) {
         this.term = term;
     }
 
-    public double getValue() {
+    public Double getValue() {
         return value;
     }
 
-    public void setValue(double value) {
+    public void setValue(Double value) {
         this.value = value;
     }
 
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/IntegrationTest/CurvePointControllerITest.java
@@ -0,0 +1,165 @@
+package com.nnk.springboot.IntegrationTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.controllers.BidListController;
+import com.nnk.springboot.controllers.CurveController;
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.domain.CurvePoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan("com.nnk.springboot.controllers")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class CurvePointControllerITest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CurveController curveController;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @BeforeEach
+    public void setup_data(){
+
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void homeCurvePoint_ShouldReturnCurvePointList() throws Exception {
+        MvcResult result = mvc.perform(get("/curvePoint/list"))
+                .andReturn();
+        ModelAndView resultModelAndView = result.getModelAndView();
+        ArrayList<CurvePoint> expectedCurvePoint = (ArrayList) resultModelAndView.getModel().get("curvePoints");
+
+        assertThat(resultModelAndView.getViewName().equals("curvePoint/list"));
+        assertThat(expectedCurvePoint.size()).isEqualTo(3);
+        assertThat(expectedCurvePoint.get(2).getCurveId()).isEqualTo(2);
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void curvePointAdd_shouldReturnCorrectURI() throws Exception {
+        MvcResult result = mvc.perform(get("/curvePoint/add"))
+                .andReturn();
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("curvePoint/add");
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void curvePointValidate_ShouldReturnUpdatedCurvePointListWithNewCurvePointList() throws Exception {
+        //ARRANGE
+        CurvePoint curvePointToAdd = new CurvePoint(1,1.0,20.0);
+
+        String curvePointToString = MAPPER.writeValueAsString(curvePointToAdd);
+
+        //ACT
+            //first request that checks the redirect send the proper URI
+        mvc.perform(post("/curvePoint/validate").with(csrf()).content(curvePointToString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(redirectedUrl("/curvePoint/list"));
+
+            //second request must return updated bidList list plus added BidList
+        MvcResult result = mvc.perform(get("/curvePoint/list"))
+                .andReturn();
+
+        ModelAndView resultModelAndView = result.getModelAndView();
+        ArrayList<CurvePoint> expectedUpdatedCurvePoint = (ArrayList) resultModelAndView.getModel().get("curvePoints");
+
+        //ASSERT
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("curvePoint/list");
+        assertThat(expectedUpdatedCurvePoint.size()).isEqualTo(4);
+        assertThat(expectedUpdatedCurvePoint.get(3).getTerm()).isEqualTo(curvePointToAdd.getTerm());
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void curvePointUpdate_ShouldReturnFormWithCurvePointInfoForUpdate() throws Exception {
+        //ARRANGE
+        String curvePointIdToUpdate = "1";
+
+        //ACT
+        MvcResult result = mvc.perform(get("/curvePoint/update/{id}", curvePointIdToUpdate)).andReturn();
+        ModelAndView resultModelAndView = result.getModelAndView();
+        CurvePoint curvePointToUpdate = (CurvePoint) resultModelAndView.getModel().get("curvePoint");
+
+        //ASSERT
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("curvePoint/update");
+        assertThat(curvePointToUpdate.getValue()).isEqualTo(8.3);
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void curvePointUpdate_ShouldSaveNewCurvePointAndReturnUpdatedCurvePointList() throws Exception {
+        //ARRANGE
+        String curvePointIdToUpdate = "3";
+        CurvePoint curvePointToUpdateWithUpdatedInfo = new CurvePoint(1,1.0,20.0);
+        curvePointToUpdateWithUpdatedInfo.setId(Integer.parseInt(curvePointIdToUpdate));
+        String curvePointToString = MAPPER.writeValueAsString(curvePointToUpdateWithUpdatedInfo);
+
+        //ACT
+            //first request that checks the request was properly redirected
+        mvc.perform(patch("/curvePoint/update/{id}", curvePointIdToUpdate).with(csrf()).content(curvePointToString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(redirectedUrl("/curvePoint/list"));
+
+            //second request must return updated BidList list
+        MvcResult result = mvc.perform(get("/curvePoint/list"))
+                .andReturn();
+
+        ModelAndView resultModelAndView = result.getModelAndView();
+        ArrayList<CurvePoint> expectedUpdatedCurvePoint = (ArrayList) resultModelAndView.getModel().get("curvePoints");
+
+        //ASSERT
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("curvePoint/list");
+        assertThat(expectedUpdatedCurvePoint.size()).isEqualTo(3);
+        assertThat(expectedUpdatedCurvePoint.get(2).getCurveId()).isEqualTo(curvePointToUpdateWithUpdatedInfo.getCurveId());
+        assertThat(expectedUpdatedCurvePoint.get(2).getValue()).isEqualTo(curvePointToUpdateWithUpdatedInfo.getValue());
+    }
+
+    @Test
+    @WithMockUser(username = "Usertest", password = "userMDP", roles = "USER")
+    public void bidListDelete_shouldReturnUpdatedListMinusDeletedBidList() throws Exception {
+        //ARRANGE
+        String curvePointIdToDelete = "1";
+
+        //ACT
+            //first request that checks the request was properly redirected
+        mvc.perform(delete("/curvePoint/delete/{id}", curvePointIdToDelete).with(csrf()))
+                .andExpect(redirectedUrl("/curvePoint/list"));;
+
+            //second request must return updated BidList list minus deleted BidList
+        MvcResult result = mvc.perform(get("/curvePoint/list"))
+                .andReturn();
+        ModelAndView resultModelAndView = result.getModelAndView();
+        ArrayList<CurvePoint> expectedUpdatedCurvePointList = (ArrayList) resultModelAndView.getModel().get("curvePoints");
+
+        //ASSERT
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("curvePoint/list");
+        assertThat(expectedUpdatedCurvePointList.size()).isEqualTo(2);
+        assertThat(expectedUpdatedCurvePointList.get(0).getValue()).isEqualTo(1.0);
+    }
+}
