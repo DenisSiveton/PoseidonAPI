1b22690461d5a40a5c54b790802de19425b8b415
Denis Siveton <denis.siveton@hotmail.com>
2023-09-17 21:28:32 +0200

    Implement integration test for RuleName controller layer

--- Poseiden-skeleton/src/test/resources/data.sql
+++ Poseiden-skeleton/src/test/resources/data.sql
@@ -2,4 +2,8 @@ insert into bid_list(Id, account, type, bid_quantity) VALUES (1, 'acc_1', 'type_
 insert into bid_list(Id, account, type, bid_quantity) VALUES (2, 'acc_2', 'type_2', 12.0);
 insert into bid_list(Id, account, type, bid_quantity) VALUES (3, 'acc_1', 'type_2', 5.5);
 
+insert into rule_name(Id, name, description, template) VALUES (1, 'name 1', 'description 1', 'template 1');
+insert into rule_name(Id, name, description, template) VALUES (2, 'name 2', 'description 2', 'template 2');
+insert into rule_name(Id, name, description, template) VALUES (3, 'name 3', 'description 3', 'template 3');
+
 insert into Users(fullname, username, password, role) VALUES ('User Test', 'Usertest', '$2a$10$3.GD7d4o4pNREE2HdbY52O5VINdNLOSe1W7LT4txvo7NxPElYcVEO', 'USER');
