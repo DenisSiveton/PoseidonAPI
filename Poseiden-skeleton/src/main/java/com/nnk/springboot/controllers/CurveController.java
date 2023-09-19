4c2d2a6b4600d4195a73c5b2c7c3a94ad5cf3cfa
Denis Siveton <denis.siveton@hotmail.com>
2023-09-19 19:30:24 +0200

    Javadoc documentation for CurvePoint controller's methods

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -12,12 +12,24 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * This class serves as a controller layer for the CurvePoint entity.
+ * It will use the CRUD methods for data persistence
+ * @author M Siveton Denis
+ * @version 1.0
+ */
 @Controller
 public class CurveController {
     // TODO: Inject Curve Point service --> DONE
     @Autowired
     private CurvePointRepository curvePointRepository;
 
+    /**
+     * This methods retrieves all CurvePoints from database and list them for the user.
+     *
+     * @param model Web UI container. Contains all the CurvePoints
+     * @return URI curvePoint/list. Show table with all CurvePoints
+     */
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
@@ -26,11 +38,30 @@ public class CurveController {
         return "curvePoint/list";
     }
 
+    /**
+     * This method returns a form to add a CurvePoint.
+     *
+     * @param curvePoint Entity that will be generated with the form then added to the Database
+     * @return URI curvePoint/add. Show form with input for new CurvePoint
+     */
     @GetMapping("/curvePoint/add")
     public String addBidForm(CurvePoint curvePoint) {
         return "curvePoint/add";
     }
 
+    /**
+     * This method checks if the data from the form are consistent and valid for a new CurvePoint.
+     * If the checks pass then add the new CurvePoint to the database.
+     *      It then retrieves all CurvePoints from database and list them in the UI for the user.
+     * If the checks fail, the user is redirected to the form for a second attempt with an error message
+     *      explaining why the request failed.
+     *
+     * @param curvePoint Entity constructed from the form. It will be added to the database
+     * @param result Form result. May contain errors if data don't comply
+     * @param model Web UI container. Contains all the CurvePoints
+     * @return URI curvePoint/list. Show table with updated CurvePoints
+     * @return In case of error : URI curvePoint/add. Returns to the form for a second attempt
+     */
     @PostMapping("/curvePoint/validate")
     public String validate(@Valid @RequestBody CurvePoint curvePoint, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Curve list -- > DONE
@@ -42,6 +73,14 @@ public class CurveController {
         return "curvePoint/add";
     }
 
+    /**
+     * This method retrieves the data of the CurvePoint form the database.
+     * It then generates a form filled with the data for modification.
+     *
+     * @param id Id of the CurvePoint the user wants to update
+     * @param model Web UI container. Contains the data of the desired CurvePoint
+     * @return URI curvePoint/add. Show form with input filled with CurvePoint's data
+     */
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get CurvePoint by Id and to model then show to the form
@@ -50,6 +89,20 @@ public class CurveController {
         return "curvePoint/update";
     }
 
+    /**
+     * This method checks if the data from the form are consistent and valid to update the CurvePoint.
+     * If the checks pass then it updates the CurvePoint into the Database using the updated data from the form.
+     *      It then retrieves all CurvePoint from database and list them in the UI for the user.
+     * If the checks fail, the user is redirected to the form for a second attempt with an error message
+     *      explaining why the request failed.
+     *
+     * @param id Id of the CurvePoint the user wants to update
+     * @param curvePoint Entity constructed from the form. It will update the existing CurvePoint in the Database
+     * @param result Form result. May contain errors if data don't comply
+     * @param model Web UI container. Contains all the CurvePoints
+     * @return URI curvePoint/list. Show table with updated CurvePoints
+     * @return In case of error : URI curvePoint/update. Returns to the form for a second attempt
+     */
     @PatchMapping("/curvePoint/update/{id}")
     public String updateCurvePoint(@PathVariable("id") Integer id, @Valid @RequestBody CurvePoint curvePoint,
                              BindingResult result, Model model) {
@@ -62,6 +115,14 @@ public class CurveController {
         return "redirect:/curvePoint/list";
     }
 
+    /**
+     * This method deletes a CurvePoint from the Database.
+     * It then retrieves all CurvePoints from database and list them in the UI for the user.
+     *
+     * @param id Id of the CurvePoint the user wants to delete
+     * @param model Web UI container. Contains all the remaining CurvePoints
+     * @return URI curvePoint/list. Show table with updated CurvePoints
+     */
     @DeleteMapping("/curvePoint/delete/{id}")
     public String deleteCurvePoint(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
