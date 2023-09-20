982283111d5f347e7b3d844fa47fe61babc0d645
Denis Siveton <denis.siveton@hotmail.com>
2023-09-20 14:32:54 +0200

    Javadoc documentation for User controller's methods

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -7,18 +7,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * This class serves as a controller layer for the User entity.
+ * It will use the CRUD methods for data persistence
+ * @author M Siveton Denis
+ * @version 1.0
+ */
 @Controller
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * This methods retrieves all Users from database and list them for the user.
+     *
+     * @param model Web UI container. Contains all the Users
+     * @return URI user/list. Show table with all Users
+     */
     @RequestMapping("/user/list")
     public String home(Model model)
     {
@@ -26,13 +35,32 @@ public class UserController {
         return "user/list";
     }
 
+    /**
+     * This method returns a form to add a User.
+     *
+     * @param user Entity that will be generated with the form then added to the Database
+     * @return URI user/add. Show form with input for new User
+     */
     @GetMapping("/user/add")
-    public String addUser(User bid) {
+    public String addUser(User user) {
         return "user/add";
     }
 
+    /**
+     * This method checks if the data from the form are consistent and valid for a new User.
+     * If the checks pass then add the new User to the database.
+     *      It then retrieves all Users from database and list them in the UI for the user.
+     * If the checks fail, the user is redirected to the form for a second attempt with an error message
+     *      explaining why the request failed.
+     *
+     * @param user Entity constructed from the form. It will be added to the database
+     * @param result Form result. May contain errors if data don't comply
+     * @param model Web UI container. Contains all the Users
+     * @return URI user/list. Show table with updated Users
+     * @return In case of error : URI user/add. Returns to the form for a second attempt
+     */
     @PostMapping("/user/validate")
-    public String validate(@Valid User user, BindingResult result, Model model) {
+    public String validate(@Valid @RequestBody User user, BindingResult result, Model model) {
         if (!result.hasErrors()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             user.setPassword(encoder.encode(user.getPassword()));
@@ -43,6 +71,14 @@ public class UserController {
         return "user/add";
     }
 
+    /**
+     * This method retrieves the data of the User from the database.
+     * It then generates a form filled with the data for modification.
+     *
+     * @param id Id of the User the user wants to update
+     * @param model Web UI container. Contains the data of the desired User
+     * @return URI user/update. Show form with input filled with User's data
+     */
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
@@ -51,8 +87,22 @@ public class UserController {
         return "user/update";
     }
 
-    @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Integer id, @Valid User user,
+    /**
+     * This method checks if the data from the form are consistent and valid to update the User.
+     * If the checks pass then it updates the User into the Database using the updated data from the form.
+     *      It then retrieves all Users from database and list them in the UI for the user.
+     * If the checks fail, the user is redirected to the form for a second attempt with an error message
+     *      explaining why the request failed.
+     *
+     * @param id Id of the User the user wants to update
+     * @param user Entity constructed from the form. It will update the existing User in the Database
+     * @param result Form result. May contain errors if data don't comply
+     * @param model Web UI container. Contains all the Users
+     * @return URI user/list. Show table with updated Users
+     * @return In case of error : URI user/update. Returns to the form for a second attempt
+     */
+    @PatchMapping("/user/update/{id}")
+    public String updateUser(@PathVariable("id") Integer id, @Valid @RequestBody User user,
                              BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "user/update";
@@ -66,7 +116,15 @@ public class UserController {
         return "redirect:/user/list";
     }
 
-    @GetMapping("/user/delete/{id}")
+    /**
+     * This method deletes a User from the Database.
+     * It then retrieves all Users from database and list them in the UI for the user.
+     *
+     * @param id Id of the User the user wants to delete
+     * @param model Web UI container. Contains all the remaining Users
+     * @return URI user/list. Show table with updated Users
+     */
+    @DeleteMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
