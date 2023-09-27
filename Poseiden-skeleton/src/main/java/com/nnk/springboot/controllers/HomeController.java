273b4674ce9a3317fe2eb9c4187244d3a898f685
Denis Siveton <denis.siveton@hotmail.com>
2023-09-27 13:57:27 +0200

    Update login success management :  redirection depending on roles

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package com.nnk.springboot.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 
 @Controller
 public class HomeController
@@ -22,10 +26,14 @@ public class HomeController
 	}
 
 	@RequestMapping("/default")
-	public String defaultAfterLogin(HttpServletRequest request) {
-		if (request.isUserInRole("ADMIN")) {
+	public String defaultAfterLogin() {
+		Collection<? extends GrantedAuthority> authorities;
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		authorities = auth.getAuthorities();
+		String myRole = authorities.toArray()[0].toString();
+		if (myRole.equals("ADMIN")) {
 			return "redirect:/admin/home";
 		}
-		return "redirect:/";
+		return "redirect:/bidList/list";
 	}
 }
