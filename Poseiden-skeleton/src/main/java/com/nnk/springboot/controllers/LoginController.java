46aab669355bba6c9073730b94341c965342f1b1
Denis Siveton <denis.siveton@hotmail.com>
2023-10-06 11:12:34 +0200

    Implement Javadoc for LoginController

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -5,15 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * This class serves as a controller layer for login and error handling process.
+ * @author M Siveton Denis
+ * @version 1.0
+ */
 @Controller
 public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * This method This method sends the user to the Application login page..
+     *
+     * @return Web UI container with its view set to "login".
+     */
     @GetMapping("/login")
     public ModelAndView login(Model model) {
         ModelAndView mav = new ModelAndView();
@@ -21,6 +30,11 @@ public class LoginController {
         return mav;
     }
 
+    /**
+     * This method retrieves all the User and shows them as a list to the user.
+     *
+     * @return Web UI container with its view set to "user/list".
+     */
     @GetMapping("/secure/article-details")
     public ModelAndView getAllUserArticles() {
         ModelAndView mav = new ModelAndView();
@@ -29,6 +43,11 @@ public class LoginController {
         return mav;
     }
 
+    /**
+     * This method is called upon an error. It sends an error message to the user for clarification.
+     *
+     * @return Web UI container with its view set to "403".
+     */
     @GetMapping("/error")
     public ModelAndView error() {
         ModelAndView mav = new ModelAndView();
